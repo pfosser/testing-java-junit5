6a1ed4b53d4450fd18b0abfbd07bc45e5520ba3c
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-06-11 09:51:14 +0200

    Lesson 50. JUnit Dependent Assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgpetclinic.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OwnerTest {
+
+	@Test
+	void dependentAssertions() {
+		Owner owner = new Owner(1L, "Joe", "Buck");
+		owner.setCity("Key West");
+		owner.setTelephone("123456789");
+
+		assertAll("Properties test", () -> {
+			assertAll("Person properties", //
+					() -> assertEquals("Joe", owner.getFirstName(), "First name did not match"), //
+					() -> assertEquals("Buck", owner.getLastName()) //
+			);
+		}, () -> {
+			assertAll("Owner properties", //
+					() -> assertEquals("Key West", owner.getCity(), "City did not match"), //
+					() -> assertEquals("123456789", owner.getTelephone()) //
+			);
+		});
+	}
+
+}
