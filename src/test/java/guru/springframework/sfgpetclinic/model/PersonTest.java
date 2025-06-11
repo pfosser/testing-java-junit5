03cb3b3678271008ac69b66cef6a89d3c7ac6ecc
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-06-11 09:38:43 +0200

    Lesson 49. JUnit Grouped Assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,32 @@
+package guru.springframework.sfgpetclinic.model;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class PersonTest {
+
+	@Test
+	void groupedAssertions() {
+		// given
+		Person person = new Person(1L, "Joe", "Buck");
+
+		// then
+		assertAll("Test props set", //
+				() -> assertEquals("Joe", person.getFirstName()), //
+				() -> assertEquals("Buck", person.getLastName()));
+	}
+
+	@Test
+	void groupedAssertionsMessage() {
+		// given
+		Person person = new Person(1L, "Joe", "Buck");
+
+		// then
+		assertAll("Test props set", //
+				() -> assertEquals("Joe", person.getFirstName(), "First name failed"), //
+				() -> assertEquals("Buck", person.getLastName(), "Last name failed"));
+	}
+
+}
