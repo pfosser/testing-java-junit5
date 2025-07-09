65f723a29323f55f09875d70b2609c7535fd98aa
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-07-09 17:59:19 +0200

    Lesson 66. Using JUnit Default Test Methods

--- src/test/java/guru/springframework/sfgpetclinic/ControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/ControllerTest.java
@@ -1,8 +1,16 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+@TestInstance(Lifecycle.PER_CLASS)
 @Tag("controller")
 public interface ControllerTest {
 
+	@BeforeAll
+	default void beforeAll() {
+		System.out.println("Lets do something here");
+	}
 }
