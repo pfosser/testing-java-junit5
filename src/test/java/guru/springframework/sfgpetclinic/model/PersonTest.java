65fa86ac83e008d77720bf9ec0921fb3f4273323
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-07-15 08:59:11 +0200

    Lesson 68. JUnit Test Dependency Injection

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -5,7 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 import guru.springframework.sfgpetclinic.ModelTest;
 
@@ -37,5 +39,10 @@ class PersonTest implements ModelTest {
 	@DisplayName("My repeated test")
 	void myRepeatedTest() {
 
+	}
+	
+	@RepeatedTest(5)
+	void myRepeatedTestWithDI(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+		System.out.println(testInfo.getDisplayName() + ": " + repetitionInfo.getCurrentRepetition());
 	}
 }
