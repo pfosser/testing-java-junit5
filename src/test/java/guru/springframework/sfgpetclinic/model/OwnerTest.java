252714a4bb766a2bd86e3c9d8f571e53caae023b
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-07-17 12:56:18 +0200

    Lesson 71. JUnit Parameterized Tests - Value Source

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import guru.springframework.sfgpetclinic.ModelTest;
 
@@ -28,8 +30,14 @@ class OwnerTest implements ModelTest {
 					() -> assertEquals("123456789", owner.getTelephone()) //
 			);
 		});
-		
+
 		MatcherAssert.assertThat(owner.getCity(), is("Key West"));
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "Spring", "Framework", "Guru" })
+	void testValueSource(String val) {
+		System.out.println(val);
 	}
 
 }
