020616d38aed61e3635b7e80f390348477c29aec
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-07-29 10:32:51 +0200

    Lesson 74. JUnit Parameterized Tests - CSV Source

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -8,6 +8,7 @@ import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -50,4 +51,10 @@ class OwnerTest implements ModelTest {
 		System.out.println(ownerType);
 	}
 
+	@DisplayName("CSV input test")
+	@ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+	@CsvSource({ "FL, 1, 1", "OH, 2, 2", "MI, 1, 1" })
+	void csvInputTest(String stateName, int val1, int val2) {
+		System.out.println(stateName + " = " + val1 + ":" + val2);
+	}
 }
