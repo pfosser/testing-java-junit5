b975073ec756e7363cd5718ee5aaf8824b553131
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-10-17 16:17:12 +0200

    Lesson 78. Unit Tests vs Integration Tests

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceTest.java
@@ -0,0 +1,20 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+// The IT suffix is recognized by Maven and marks the class as an integration test
+class PetTypeSDJpaServiceIT {
+
+	@BeforeEach
+	void setUp() {
+		
+	}
+	
+	@Test
+	void findAllTest() {
+		
+	}
+}
